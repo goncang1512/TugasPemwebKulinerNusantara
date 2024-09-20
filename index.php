971658386545538405a67fb6257cd9b4ad7d115e
goncang1512 <samuderanst@gmail.com>
2024-09-20 18:45:59 +0700

    fix: ganti kata jadi HOME PAGE DI SINI AJA

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <?php include_once("./layouts/navbar.php");?>
     </header>
     <h1>Hello World</h1>
-    <h1>Nama saya goncang samudera nasution</h1>
+    <h1>HOME PAGE DI SINI AJA</h1>
     
     <?php include_once("./layouts/footer.php");?>
     <?php include_once("./layouts/script.php");?>
