71b96471e6baa79831d95b629655c5f637a8e299
goncang1512 <132435801+goncang1512@users.noreply.github.com>
2024-09-20 18:33:28 +0700

    refactor: Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <header>
         <?php include_once("./layouts/navbar.php");?>
     </header>
-    <h1>Apa kabar</h1>
+    <h1>Hello World</h1>
     
     <?php include_once("./layouts/footer.php");?>
     <?php include_once("./layouts/script.php");?>
