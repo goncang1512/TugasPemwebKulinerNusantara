364abdb7b5886766c73b09859ffabc49ac9ea81f
goncang1512 <samuderanst@gmail.com>
2024-09-20 17:32:36 +0700

    intiliaze php project

---
+++ pages/profil.php
@@ -0,0 +1,17 @@
+<?php
+include_once("../config/helper.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once("../layouts/head.php");?>
+</head>
+<body>
+    <h1>Profile</h1>
+
+    <?php include_once("../layouts/script.php");?>
+</body>
+</html>
