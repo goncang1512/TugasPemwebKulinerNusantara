bc5f605eee3f077445ec4c33bccd58a40b040cb5
siregaramri <khoirulamrisiregaramri@gmail.com>
2024-09-26 16:02:18 +0700

    halaman login dan register

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -1,3 +1,10 @@
 <main>
-    <h1>LOGIN</h1>
+    <div class="container">
+        <img class="image" src="<?= BASE_URL."assets/image/logo_web_kuliner-removebg-preview.png"?>" alt="Logo Resep Nusantara">
+        <form class="registrasi">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <button type="submit">Daftar</button>
+        </form> 
+    </div> 
 </main>
--- src/views/auth/register.view.php
+++ src/views/auth/register.view.php
@@ -1,3 +1,12 @@
 <main>
-    <h1>REGISTER</h1>
+    <div class="container">
+        <img class="image" src="<?= BASE_URL."assets/image/logo_web_kuliner-removebg-preview.png"?>" alt="Logo Resep Nusantara">
+        <form class="registrasi">
+            <input type="text" placeholder="Username">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <input type="password" placeholder="Konfirmasi password">
+            <button type="submit">Daftar</button>
+        </form> 
+    </div> 
 </main>
