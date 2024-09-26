300df1828785fa383c90415e63e474a21192b51b
ferdyariyansyah <ferdyariyansyah690@gmail.com>
2024-09-26 16:54:19 +0700

    homepage

--- src/includes/head.php
+++ src/includes/head.php
@@ -1,18 +1,20 @@
 <?php
     $pathname = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    $excluded_paths = ["/resep_nusantara/pages/register/", "/resep_nusantara/pages/login/"];
+    $excluded_paths = ["/TugasPemwebKulinerNusantara/pages/register/", "/TugasPemwebKulinerNusantara/pages/login/"];
 ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="<?= BASE_URL . "assets/css/index.css" ?>">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 <!-- CSS PAGES -->
-<?php if (strpos($pathname, '/resep_nusantara/pages/detail/') === 0): ?>
+<?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/detail/') === 0): ?>
     <link rel="stylesheet" href="<?= BASE_URL . "assets/css/detail.css" ?>">
 <?php endif; ?>
 
-<?php if (strpos($pathname, '/resep_nusantara/pages/profile/') === 0): ?>
+<?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/profile/') === 0): ?>
     <link rel="stylesheet" href="<?= BASE_URL . "assets/css/profile.css" ?>">
 <?php endif; ?>
 
@@ -20,7 +22,7 @@
     <link rel="stylesheet" href="<?= BASE_URL . "assets/css/auth.css" ?>">
 <?php endif; ?>
 
-<?php if ($pathname === "/resep_nusantara/" || $pathname === "/resep_nusantara/index.php"): ?>
+<?php if ($pathname === "/TugasPemwebKulinerNusantara/" || $pathname === "/TugasPemwebKulinerNusantara/index.php"): ?>
     <link rel="stylesheet" href="<?= BASE_URL."assets/css/home.css"?>">
 <?php endif;?>
 
