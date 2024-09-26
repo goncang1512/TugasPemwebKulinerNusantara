dda16e7486b6a41aac5b1721915ec2901fb65d63
khoirulamris <khorulamri12345@gmail.com>
2024-09-26 17:30:35 +0700

    login dan register

--- src/includes/head.php
+++ src/includes/head.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" href="<?= BASE_URL . "assets/css/index.css" ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 <!-- CSS PAGES -->
 <?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/detail/') === 0): ?>
     <link rel="stylesheet" href="<?= BASE_URL . "assets/css/detail.css" ?>">
@@ -22,6 +23,10 @@
     <link rel="stylesheet" href="<?= BASE_URL . "assets/css/auth.css" ?>">
 <?php endif; ?>
 
-<?php if ($pathname === "/TugasPemwebKulinerNusantara" || $pathname === "/TugasPemwebKulinerNusantara/index.php"): ?>
+<?php if ($pathname === "/TugasPemwebKulinerNusantara/" || $pathname === "/TugasPemwebKulinerNusantara/index.php"): ?>
     <link rel="stylesheet" href="<?= BASE_URL."assets/css/home.css"?>">
-<?php endif; ?>
+<?php endif;?>
+
+<?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/upload/') === 0):?>
+    <link rel="stylesheet" href="<?= BASE_URL."assets/css/upload.css"?>">
+<?php endif?>
