e9f7ef6f3141b676faddc772ea48bf81944e3739
ReyhanArista <kerenreyhan9@gmail.com>
2024-09-26 16:02:21 +0700

    Halaman Detail

--- src/views/detail/detail.view.php
+++ src/views/detail/detail.view.php
@@ -1,3 +1,80 @@
 <main>
-    <h1>DETAIL PAGES</h1>
+    <h1>BIKA AMBON</h1>
+    <div class="atas">
+        <img class="gambar1"src="https://c1.wallpaperflare.com/preview/428/552/953/bika-ambon-bika-plate-slice.jpg" alt="Bika Ambon">
+        <p>Bika Ambon adalah kue tradisional Indonesia yang terkenal dengan tekstur berongga dan lembut di bagian dalam serta lapisan luar yang agak renyah. Kue ini terbuat dari bahan-bahan seperti tepung tapioka, santan, telur, gula, dan ragi. Rasa khasnya berasal dari perpaduan antara manis, gurih, dan aroma daun pandan atau jeruk yang sering digunakan dalam adonannya.
+        </p>
+    </div>
+    <div style="border-top: 2px solid black; width: 100%; margin: 20px 0;margin-top:50px"></div>
+
+    <div class="container-timer">
+        <div style="height: 35px; width:100%; background-color: green;"></div>
+        <div style="padding: 10px;">
+            <table style="width:100%">
+                <tr>
+                    <th>Waktu Persiapan :</th>
+                    <th>Waktu Memasak :</th>
+                    <th>Total Waktu :</th>
+                </tr>
+                <tr>
+                    <th>30mins</th>
+                    <th>30mins</th>
+                    <th>30mins</th>
+                </tr>
+                <tr>
+                    <th style="padding-top:50px">Porsi :</th>
+                    <th style="padding-top:50px">Kesulitan :</th>
+                </tr>
+                <tr>
+                    <th>6</th>
+                    <th>Mudah</th>
+                </tr>
+            </table>
+        </div>
+        <div style="padding:20px 50px">
+            <div style="border-top: 2px solid black; width: 100%;"></div>
+        </div>
+        <div class="button">
+            <h6 style="font-size:20px">00:30:05</h6>
+            <button class="btn"><i class="bi bi-stopwatch"></i>Mulai Waktu</button>
+        </div>
+    </div>
+
+    <div class="container2">
+        <div class="section">
+            <h2>Bahan-Bahan</h2>
+            <ul>
+                <li>200 gram tepung sagu</li>
+                <li>250 gram gula pasir</li>
+                <li>10 kuning telur</li>
+                <li>250 ml santan kental dari 1 butir kelapa</li>
+                <li>50 ml air kelapa</li>
+                <li>1/2 sendok teh garam</li>
+                <li>15 lembar daun jeruk purut, buang tulang daunnya</li>
+                <li>1/4 sendok teh kunyit bubuk</li>
+                <li>2 sendok makan margarin</li>
+                <li>1 bungkus (11 gram) ragi instan</li>
+                <li>15 gram gula pasir</li>
+                <li>10 gram tepung terigu</li>
+                <li>60 ml air hangat</li>
+            </ul>
+        </div>
+        <div class="section instructions">
+            <h2>Cara Membuat</h2>
+            <ol>
+                <li>Buat biangnya terlebih dahulu. Campurkan semua bahan biang, diamkan selama 15 menit. Sisihkan.</li>
+                <li>Masak santan bersama daun jeruk sampai mendidih, dinginkan. Setelah dingin, campur dengan tepung sagu.</li>
+                <li>Kocok telur bersama gula pasir dengan speed rendah sampai gula larut.</li>
+                <li>Setelah 15 menit, campurkan biang dengan campuran tepung sagu. Masukkan campuran tersebut ke kocokan telur dan kunyit bubuk. Tambahkan margarin, aduk rata. Diamkan adonan selama tiga jam.</li>
+                <li>Tuang adonan yang telah didiamkan ke dalam cetakan ukuran 20×10×7 cm yang sudah dioles minyak dan dalam kondisi panas.</li>
+                <li>Panggang di atas pasir yang sudah dipanaskan sampai gelembung berhenti dan berlubang-lubang.</li>
+                <li>Lanjutkan memanggang bika ambon di oven dengan api atas suhu 190°C dengan pintu oven terbuka.</li>
+            </ol>
+        </div>
+    </div>
+
+<div>
+    <video src="RESEP BIKA AMBON BESARANG DAN ANTI GAGAL.mp4"></video>
+</div>
+
 </main>
