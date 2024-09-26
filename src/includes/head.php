47c2c4ebac8ffd5a76d89ac935ae6e061bd3e2b2
goncang1512 <samuderanst@gmail.com>
2024-09-26 16:02:25 +0700

    halaman profile

--- src/includes/head.php
+++ src/includes/head.php
@@ -5,8 +5,8 @@
 ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<link rel="stylesheet" href="<?= BASE_URL . "assets/css/index.css" ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+<link rel="stylesheet" href="<?= BASE_URL . "assets/css/index.css" ?>">
 
 <!-- CSS PAGES -->
 <?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/detail/') === 0): ?>
@@ -25,3 +25,6 @@
     <link rel="stylesheet" href="<?= BASE_URL."assets/css/home.css"?>">
 <?php endif;?>
 
+<?php if (strpos($pathname, '/TugasPemwebKulinerNusantara/pages/upload/') === 0):?>
+    <link rel="stylesheet" href="<?= BASE_URL."assets/css/upload.css"?>">
+<?php endif?>
--- src/views/upload/upload.view.php
+++ src/views/upload/upload.view.php
@@ -1,3 +1,75 @@
-<main>
-    <h1>UPLOAD PAGE</h1>
+<main class="main-konten">
+    <form class="">
+        <div class="image-resep">
+            <img class="input-img" src="https://media.istockphoto.com/id/1934499650/photo/assorted-malaysian-favorite-foods.webp?a=1&b=1&s=612x612&w=0&k=20&c=eocWmcUaf4o7F7cNkdGPVIGz1Gmcm7q73VrNDM4RzUc=" alt="">
+            <div class="table-input-judul"> 
+                <div class="input-group flex-nowrap" style="display: flex; flex-direction: column; gap: 10px;">
+                    <label for="judul">Judul</label>
+                    <input type="text" id="judul" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                </div>
+                <div class="input-group flex-nowrap" style="display: flex; flex-direction: column; gap: 10px; height: 100%;">
+                    <label for="deskripsi">Deskripsi</label>
+                    <textarea name="" id="deskripsi" type="text" class="form-control" placeholder="Deskripsi" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;min-height: 150px;"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <div class="" style="padding-top: 20px;">
+            <table style="width: 100%;">
+                <tr>
+                    <td>Waktu persiapan</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Waktu persiapan" aria-label="Waktu persiapan" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Waktu Memasak</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Waktu Memasak" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Total Waktu</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Total Waktu" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Porsi</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Porsi" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Kesulitan</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Kesulitan" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Asal makanan</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Asal makanan" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Kategori makanan</td>
+                    <td>
+                        <input type="text" id="judul" class="form-control" placeholder="Kategori makanan" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;">
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="container-bahan" style="padding-top: 60px;">
+            <div class="input-group flex-nowrap" style="display: flex; flex-direction: column; gap: 10px; height: 100%;">
+                <label for="deskripsi">Bahan bahan</label>
+                <textarea name="" id="deskripsi" type="text" class="form-control" placeholder="Bahan bahan" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;min-height: 150px;"></textarea>
+            </div>
+            <div class="input-group flex-nowrap" style="display: flex; flex-direction: column; gap: 10px; height: 100%;">
+                <label for="deskripsi">Langkah langkah</label>
+                <textarea name="" id="deskripsi" type="text" class="form-control" placeholder="Langkah langkah" aria-label="Username" aria-describedby="addon-wrapping" style="width: 100%; border-radius: 5px;min-height: 150px;"></textarea>
+            </div>
+        </div>
+    </form>
 </main>
