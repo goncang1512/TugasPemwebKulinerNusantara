4b938059b5bc159b1f105b3256d11ed3bd122695
goncang1512 <samuderanst@gmail.com>
2024-10-05 00:43:17 +0700

    feat: menambahkan tabel rating

--- src/app/migrate.php
+++ src/app/migrate.php
@@ -2,6 +2,7 @@
 
 include_once("./app.php");
 
+use Model\Rating;
 use Model\Resep;
 use Model\Save;
 use Model\User;
@@ -9,12 +10,14 @@ use Model\User;
 $resep = new Resep();
 $user = new User();
 $save = new Save();
+$rating = new Rating();
 
 $resUser = $user->tableUser();
 $result = $resep->createTables();
 $resSave = $save->tableSave();
+$ratingCr = $rating->ratingTable();
 
-if ($result && $resUser && $resSave) {
+if ($result && $resUser && $resSave && $ratingCr) {
     echo "Tabel berhasil dibuat.";
 } else {
     echo "Gagal membuat tabel.";
