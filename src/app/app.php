9fe49ed48d2914838dfdb070465a7fc3a96f89f9
goncang1512 <samuderanst@gmail.com>
2024-10-20 21:57:38 +0700

    push

--- src/app/app.php
+++ src/app/app.php
@@ -1,21 +1,21 @@
 <?php
-
 $pathname = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 define("APP_NAME", dirname(__FILE__)."/../");
 define("BASE_URL","/TugasPemwebKulinerNusantara/");
 define("APP_PATH", dirname(__DIR__) . '/');
+define("ROUTES", dirname(__DIR__,2));
 
-include_once(__DIR__."/../../vendor/autoload.php");
+include_once(ROUTES."/vendor/autoload.php");
 include_once(APP_PATH . "app/function.php");
 include_once(APP_PATH  . 'app/env.php');
 
 use App\DotEnv;
 
 if ($pathname === BASE_URL || $pathname === BASE_URL."index.php") {
-    (new DotEnv('./.env'))->load();
+    (new DotEnv(ROUTES.'/.env'))->load();
 } else if($pathname === BASE_URL."api/" || $pathname === BASE_URL."api/index.php") {
-    (new DotEnv('../.env'))->load();
+    (new DotEnv(ROUTES.'/.env'))->load();
 } else {
-    (new DotEnv('../../.env'))->load();
+    (new DotEnv(ROUTES.'/.env'))->load();
 }
