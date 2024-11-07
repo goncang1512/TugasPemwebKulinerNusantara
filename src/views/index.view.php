280e419d6e0b87698a475bebad679573496a9376
goncang1512 <samuderanst@gmail.com>
2024-11-07 12:58:58 +0700

    feat: perubah di homepage untuk dari gambar jadi html dan perubahan pada navbar saat di scroll maka background nya jadi putih

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -29,33 +29,13 @@ $asal_makanan = [
 ?>
 
 <main class="utama">
-    <div style="overflow-x: hidden;">
+    <!-- <div style="overflow-x: hidden;">
         <img style="width: 100vw" src="<?= BASE_URL."assets/images/home.png"?>" alt="">
+    </div> -->
+
+    <div>
+        <?php component('hero')?>
     </div>
-<!-- <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" style="display: none;">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="<?= BASE_URL. "assets/images/beranda/11web.png"?>" class="imagecarausel d-block w-100" alt="">
-    </div>
-    <div class="carousel-item">
-        <img src="<?= BASE_URL. "assets/images/beranda/12web'.png"?>" class="imagecarausel d-block w-100" alt="Bika Ambon">
-    </div>
-    <div class="carousel-item">
-        <img src="<?= BASE_URL. "assets/images/beranda/13web.png"?>" class="imagecarausel d-block w-100" alt="Bika Ambon">
-    </div>
-    <div class="carousel-item">
-        <img src="<?= BASE_URL. "assets/images/beranda/14web.png"?>" class="imagecarausel d-block w-100" alt="Bika Ambon">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div> -->
 
     <div class="Welcome">
         <h2><strong>SELAMAT DATANG DI KULINER NUSANTARA</strong></h2>
