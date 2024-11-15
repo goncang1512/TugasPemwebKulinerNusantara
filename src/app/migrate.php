c365950fccebcbeaf21f245880fbded0d9a5cfeb
goncang1512 <samuderanst@gmail.com>
2024-11-15 23:14:08 +0700

    feat: comment nya sudah terkoneksi ke database

--- src/app/migrate.php
+++ src/app/migrate.php
@@ -2,6 +2,7 @@
 
 include_once("./app.php");
 
+use Model\Comment;
 use Model\Rating;
 use Model\Resep;
 use Model\Save;
@@ -11,13 +12,15 @@ $resep = new Resep();
 $user = new User();
 $save = new Save();
 $rating = new Rating();
+$comment = new Comment();
 
 $resUser = $user->tableUser();
 $result = $resep->createTables();
 $resSave = $save->tableSave();
 $ratingCr = $rating->ratingTable();
+$commentMod = $comment->createComment();
 
-if ($result && $resUser && $resSave && $ratingCr) {
+if ($result && $resUser && $resSave && $ratingCr && $commentMod) {
     echo "Tabel berhasil dibuat.";
 } else {
     echo "Gagal membuat tabel.";
